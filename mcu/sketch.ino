288d150aa4502fd2fa055e7f006b46046685c0ad
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2025-11-30 09:28:47 +0100

    Implement set_matrix_frame and setup functions
    Add matrix frame handling and Bridge RPC functionality

--- mcu/sketch.ino
+++ mcu/sketch.ino
@@ -1,1 +1,33 @@
+#include <Arduino.h>
+#include <Arduino_RouterBridge.h>
+
+extern "C" void matrixWrite(const uint32_t *buf);
+extern "C" void matrixBegin();
+
+static uint32_t currentFrame[4];
+
+// Fonction appellee depuis Linux via Bridge.call("set_matrix_frame", ...)
+void set_matrix_frame(uint32_t w0, uint32_t w1, uint32_t w2, uint32_t w3) {
+  currentFrame[0] = w0;
+  currentFrame[1] = w1;
+  currentFrame[2] = w2;
+  currentFrame[3] = w3;
+
+  matrixWrite(currentFrame);
+}
+
+void setup() {
+  // Initialisation de la matrice
+  matrixBegin();
+
+  // Initialisation de la Bridge RPC cote MCU
+  Bridge.begin();
+
+  // On expose la fonction 'set_matrix_frame' au monde Python
+  Bridge.provide("set_matrix_frame", set_matrix_frame);
+}
+
+void loop() {
+  // Rien a faire ici, le MCU attend simplement les appels Bridge
+}
 
